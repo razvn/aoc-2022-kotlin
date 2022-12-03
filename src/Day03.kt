7c750a885070279d858e9627026515090f80beb2
Razvan <@razvn>
2022-12-03 16:45:05 +0100

    refactor: day 3 - using a commun tailrec function for getting the communChars

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,17 +1,18 @@
 
 fun main() {
     val day = "Day03"
+
     fun part1(input: List<String>): Int = input.fold(emptyList<Char>()) { acc, line ->
-        val first = line.substring(0, line.length / 2)
-        val second = line.substring(line.length / 2)
-        val commun = first.toSet().intersect(second.toSet())
+        val commun = communChars(emptySet(), line.chunked(line.length/2))
         acc + commun
     }.let { value(it)}
 
     fun part2(input: List<String>): Int = input.chunked(3).fold(0) { acc, group ->
-        val commun = group[0].toSet().intersect(group[1].toSet()).intersect(group[2].toSet())
+        val commun = communChars(emptySet(), group)
         acc + value(commun)
     }
+
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("${day}_test")
@@ -25,6 +26,20 @@ fun main() {
     println(part2(input))
 }
 
+private tailrec fun communChars(commun: Set<Char>, remain: List<String>): Set<Char> = when {
+    commun.isEmpty() -> when {
+        remain.size > 1 -> {
+            val newCommun = remain[0].toSet().intersect(remain[1].toSet())
+            communChars(newCommun, remain.drop(2))
+        }
+        else -> emptySet()
+    }
+    remain.isEmpty() -> commun
+    else -> {
+        val newCommon = commun.intersect(remain.first().toSet())
+        communChars(newCommon, remain.drop(1))
+    }
+}
 private fun value(items: Collection<Char>): Int = items.fold(0) { acc, char ->
     val baseCharCode = if (char in 'a'..'z') 'a'.code - 1 else 'A'.code - 1 - 26
     val charValue = char.code - baseCharCode
