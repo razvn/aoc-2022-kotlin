73d1117b49111e5044273767a120142b5797a67b
Razvan <@razvn>
2022-12-11 20:37:30 +0100

    Day 11 - implementation

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,77 +1,97 @@
-import java.lang.Exception
-
 fun main() {
-    val day = "Day10"
+    val day = "Day11"
     fun part1(input: List<String>): Int {
-        val values = loadValues(input)
-        val keep = valuesToKeep(values, 20, 60, 100, 140, 180, 220)
-        // println(keep)
-        return keep.map { (k, v) -> k * v }.sum()
+        val monkeys = loadValues(input)
+        // println("Start --->")
+        // monkeys.forEach(::println)
+        (1..20).forEach {
+            monkeys.forEach { (_, m) ->
+                m.items.forEach { item ->
+                    m.inspectedItems ++
+                    val newWorry = m.operation.applyOperation(item) / 3
+                    if (newWorry % m.test == 0L) {
+                        monkeys[m.testTrue]!!.items.add(newWorry)
+                    } else {
+                        monkeys[m.testFalse]!!.items.add(newWorry)
+                    }
+                }
+                m.items.clear()
+            }
+            // println("After $it --->")
+            // monkeys.forEach(::println)
+        }
+        return monkeys.values.sortedByDescending { it.inspectedItems }.take(2).let {
+            it.first().inspectedItems * it.last().inspectedItems
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        val nbChars = 40
-        val fillChar = "."
-
-        val mapValues = loadValues(input)
-        var x: Int
-        var sprite: String
-        var line = ""
-        val lines = (1..240).mapNotNull { cycle ->
-            x = mapValues.xValue(cycle)
-            val spritePrefix = if (x < 1) "" else fillChar.repeat(x - 1)
-            val fillPostfix = fillChar.repeat(nbChars)
-
-            sprite = ("$spritePrefix###$fillPostfix").take(nbChars)
-
-            val idx = (cycle - 1) % nbChars
-            val draw = sprite[idx]
-            line += draw
-            // println("Cycle: \t$cycle \t| \tx: \t$x \t| \tidx: \t$idx \t| \tdraw: \t'$draw' \t| \tsprite: \t'$sprite' \t| \tcurrent: \t'$s'")
-            if (cycle % nbChars == 0) {
-                val fullLine = line
-                line = ""
-                fullLine
-            } else null
+    fun part2(input: List<String>): Long {
+        val monkeys = loadValues(input)
+        // println("Start --->")
+        // monkeys.forEach(::println)
+        val div = monkeys.values.fold(1) { acc, value -> acc * value.test }
+        (1..10000).forEach {
+            monkeys.forEach { (_, m) ->
+                m.items.forEach { item ->
+                    m.inspectedItems ++
+                    val newWorry = m.operation.applyOperation(item) % div
+                    if (newWorry % m.test == 0L) {
+                        monkeys[m.testTrue]!!.items.add(newWorry)
+                    } else {
+                        monkeys[m.testFalse]!!.items.add(newWorry)
+                    }
+                }
+                m.items.clear()
+            }
+            // println("After $it --->")
+            // monkeys.forEach(::println)
         }
-        lines.forEach { println(it.replace(fillChar, " ")) }
-        return 0
+        return monkeys.values.sortedByDescending { it.inspectedItems }.take(2).let {
+            it.first().inspectedItems.toLong() * it.last().inspectedItems.toLong()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("${day}_test")
     // println(part1(testInput))
-    println(part2(testInput))
-    check(part1(testInput) == 13140)
-    // check(part2(testInput) == 36)
+    // println(part2(testInput))
+    check(part1(testInput) == 10605)
+    check(part2(testInput) == 2713310158)
 
     val input = readInput(day)
     println(part1(input))
     println(part2(input))
 }
 
-private fun loadValues(input: List<String>): Map<Int, Int> {
-    var x = 1
-    var cycle = 1
-    return mapOf(1 to 1) + input.map {
-        when {
-            it == "noop" -> cycle++
-            it.startsWith("add") -> {
-                val cmd = it.split(" ")
-                x += cmd[1].toInt()
-                cycle += 2
-            }
-        }
-        cycle to x
-    }.toMap()
-}
+private enum class Op { PLUS, TIMES }
+private data class Operation(val op: Op, val arg2: Long?) {
+    constructor(params: List<String>) : this(if (params[0] == "+") Op.PLUS else Op.TIMES, params[1].toLongOrNull())
 
-private fun valuesToKeep(map: Map<Int, Int>, vararg cycle: Int): Map<Int, Int> {
-    return cycle.associateWith {
-        // println("Getting: $it - ${map[it]} - ${map[it - 1]} - ${map[it - 2]}")
-        val response = map[it] ?: map[it - 1]
-        response ?: throw Exception("Both $it and $it - 1 are null - can't be true")
+    fun applyOperation(value: Long): Long = when(op) {
+        Op.PLUS -> value + (arg2 ?: value)
+        Op.TIMES -> value * (arg2 ?: value)
     }
 }
 
-private fun Map<Int, Int>.xValue(idx: Int) = this[idx] ?: this[idx - 1] ?: throw Exception("There should be a value for '$idx'")
+private data class Monkey(
+    val id: Int,
+    val items: MutableList<Long>,
+    val operation: Operation,
+    val test: Int,
+    val testTrue: Int,
+    val testFalse: Int,
+    var inspectedItems: Int = 0
+)
+
+private fun loadValues(input: List<String>): MutableMap<Int, Monkey> {
+    return input.chunked(7).map { monkey ->
+        val id = monkey[0].substringAfter("Monkey ").dropLast(1).toInt()
+        val items = monkey[1].substringAfter("Starting items: ").split(", ").map { it.toLong() }
+        val operation = monkey[2].substringAfter("Operation: new = old ").split(" ")
+        val test = monkey[3].substringAfter("Test: divisible by ").toInt()
+        val testTrue = monkey[4].substringAfter("If true: throw to monkey ").toInt()
+        val testFalse = monkey[5].substringAfter("If false: throw to monkey ").toInt()
+
+        id to Monkey(id, items.toMutableList(), Operation(operation), test, testTrue, testFalse)
+    }.toMap().toMutableMap()
+}
